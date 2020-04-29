bfe75ba756e7142403e55a48d8926bf6391134d3
rohto <rohto111117@gmail.com>
2020-04-29 18:20:34 +0900

    local deploy complete

--- src/main.js
+++ src/main.js
@@ -3,13 +3,29 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAeFiFA1ViGVDMfWMwKB-hTK_fnQtViSHU",
+  authDomain: "vue-third-pj.firebaseapp.com",
+  databaseURL: "https://vue-third-pj.firebaseio.com",
+  projectId: "vue-third-pj",
+  storageBucket: "vue-third-pj.appspot.com",
+  messagingSenderId: "211841787879",
+  appId: "1:211841787879:web:2ded6053d64b43d18e3f42",
+  measurementId: "G-1L4KELKBRZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  components: { App }
 })
